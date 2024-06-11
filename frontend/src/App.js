c7ed52c48e391f6cec81ffdddd275d94a0b1082d
Adhithiyan R <100017167+Adhi2624@users.noreply.github.com>
2024-06-12 01:41:07 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,9 @@ import AdminBlog from './components/blog/AdminBlog';
 // import Dashboard from './pages/dashboard';
 import Navbarr from './components/nav';
 import SignupQuestions from './components/SignupQuestions';
+import  StudentsRoute  from './routes/studentsRoute';
+import './css/style.css';
+import MiRoute from './routes/mi';
 
 function App() {
     return (
@@ -17,6 +20,8 @@ function App() {
                 {/* <Navbarr /> */}
                 <Routes>
                     <Route path="/" element={<Home />} />
+    <Route path='/student/*' element={<StudentsRoute />} />
+        <Route path='/mi/*' element={<MiRoute />} /
                     <Route path="/login" element={<Login />} />
                     <Route path="/newReg" element={<SignUP />} />
                     <Route path="/signUp" element={<SignupQuestions />} />

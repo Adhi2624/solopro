d8d69e293cca9a0b43272bdc87340e934f2f513b
Adhithiyan R <adhithiyan.21it@sonatech.ac.in>
2024-07-11 12:39:03 +0530

    PUSH

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,8 +8,8 @@ const Mentor = require('../models/Mentor');
 const Student = require('../models/Student');
 const Investor = require('../models/Investor');
 
-const Entrepreneur = require('../models/entrepreneur');
-const User = require('../models/User'); 
+//const Entrepreneur = require('../models/entrepreneur');
+//const User = require('../models/User'); 
 const sendWelcomeEmail = require('../mailtemplates/registerMail');
 const sendMeetingEmail = require('../mailtemplates/meetingconfirm');
 
